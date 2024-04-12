9e8f684787a0618f0b916e14802b1798bb6f3e82
Vicente Vega <vicente.vega@usach.cl>
2024-04-12 09:13:28 -0400

    feat: filtros en busqueda de features

--- frontend/src/components/FeatureTable.vue
+++ frontend/src/components/FeatureTable.vue
@@ -5,18 +5,38 @@
     :items="items"
     :items-per-page="itemsPerPage"
   >
-  <template v-slot:[`item.actions`]="{ item }">
-      <v-btn
-        size="small"
-        @click="deleteItem(item)"
-      >
-        Crear comentario
-      </v-btn>
+    <template v-slot:top>
+      <v-text-field
+        :model-value="page"
+        class="pa-2"
+        label="Page"
+        type="number"
+        hide-details
+        @update:model-value="updateFeaturesByPage($event)"
+      ></v-text-field>
+
+      <v-text-field
+        :model-value="itemsPerPage"
+        class="pa-2"
+        label="Items per page"
+        max="15"
+        min="-1"
+        type="number"
+        hide-details
+        @update:model-value="updateFeaturesByItemsPerPage($event)"
+      ></v-text-field>
+
+      <v-select
+        v-model="selectedFilters"
+        :items="filters"
+        label="Mag Type"
+        multiple
+        persistent-hint
+      ></v-select>
     </template>
-    <template v-slot:bottom>
-      <div class="text-center pt-2">
-        <v-pagination v-model="page" :length="pageCount"></v-pagination>
-      </div>
+
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-btn size="small" @click="deleteItem(item)"> Crear comentario </v-btn>
     </template>
   </v-data-table>
 </template>
@@ -28,84 +48,122 @@ export default {
   data() {
     return {
       page: 1,
-      itemsPerPage: 5,
+      itemsPerPage: 20,
       headers: [
         {
-          align: 'center',
+          align: "center",
           title: "External ID",
           key: "attributes.external_id",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Título",
           key: "attributes.title",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Lugar",
           key: "attributes.place",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Fecha",
           key: "attributes.time",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Tsunami",
           key: "attributes.tsunami",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Tipo Mag",
           key: "attributes.mag_type",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Longitud",
           key: "attributes.coordinates.longitude",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Latitud",
           key: "attributes.coordinates.latitude",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Magnitud",
           key: "attributes.magnitude",
         },
         {
-          align: 'center',
+          align: "center",
           title: "URL",
           key: "links.external_url",
         },
         {
-          align: 'center',
+          align: "center",
           title: "Acción",
           key: "actions",
           sortable: false,
         },
       ],
+      filters: ["md", "ml", "ms", "mw", "me", "mi", "mb", "mlg"],
+      selectedFilters: [],
       items: [],
     };
   },
 
   async created() {
-    let result = await featureService.getFeatures();
+    let result = await featureService.getFeatures(this.page, this.itemsPerPage);
     this.items = result.data;
   },
 
-  computed: {
-    pageCount() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
-    },
+  watch: {
+    selectedFilters(newValue, oldValue) {
+      console.log(oldValue)
+      this.updateFeaturesByMagType(newValue);
+    }
   },
 
   methods: {
-    deleteItem (item) {
-      console.log(item)
-    }
-  }
+    deleteItem(item) {
+      console.log(item);
+    },
+    async updateFeaturesByItemsPerPage(itemsPerPage) {
+      console.log(itemsPerPage);
+      if (itemsPerPage) {
+        this.itemsPerPage = parseInt(itemsPerPage, 10);
+        let result = await featureService.getFeatures(
+          this.page,
+          this.itemsPerPage
+        );
+        this.items = result.data;
+      }
+    },
+    async updateFeaturesByPage(page) {
+      console.log(page);
+      if (page) {
+        this.page = parseInt(page, 10);
+        let result = await featureService.getFeatures(
+          this.page,
+          this.itemsPerPage
+        );
+        this.items = result.data;
+      }
+    },
+
+    async updateFeaturesByMagType(magType) {
+      console.log(magType);
+      if(magType){
+        const magTypeString = magType.toString();
+        let result = await featureService.getFeatures(
+          this.page,
+          this.itemsPerPage,
+          magTypeString
+        );
+        this.items = result.data;
+      }
+    },
+  },
 };
 </script>
--- frontend/src/services/featureService.js
+++ frontend/src/services/featureService.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 const featureService = {
-    getFeatures: async () => {
+    getFeatures: async (page, itemsperpage, magType) => {
         try {
+            let endpoint = "/features?per_page="+itemsperpage+"&page="+page;
+            if (magType){
+                endpoint = endpoint + "&magType=" + magType;
+            }
             const instance = axios.create({ baseURL: "http://localhost:8080/"})
-            const result = await instance.get("/features?per_page=10&page=1")
+            const result = await instance.get(endpoint)
             return result.data;
         } catch (error) {
             throw new Error(error);
